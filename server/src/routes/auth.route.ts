1d8fda03ae76ef790a88ccc47583a99a1e391eab
Saad <saad_eew@hotmail.com>
2025-07-20 21:09:12 +0500

    add type def

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
 import { check, login, logout, register } from "../controllers/auth.controller";
+import type { JwtVariables } from "hono/jwt";
+type Variables = JwtVariables;
 
-const authRoutes = new Hono();
+const authRoutes = new Hono<{ Variables: Variables }>();
 
 authRoutes.post("/register", register);
 authRoutes.post("/login", login);
