89c164017f70274ddf19c0ee468afeb5a9a456a1
Saad <saad_eew@hotmail.com>
2025-07-22 00:10:28 +0500

    add aduthmiddleware to routes

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import { Hono } from "hono";
 import { check, login, logout, register } from "../controllers/auth.controller";
 import type { JwtVariables } from "hono/jwt";
+import { authMiddleware } from "../middleware/auth.middleware";
 type Variables = JwtVariables;
 
 const authRoutes = new Hono<{ Variables: Variables }>();
 
 authRoutes.post("/register", register);
 authRoutes.post("/login", login);
-authRoutes.post("/logout", logout);
-authRoutes.get("/check", check);
+authRoutes.post("/logout", authMiddleware, logout);
+authRoutes.get("/check", authMiddleware, check);
 
 export default authRoutes;
