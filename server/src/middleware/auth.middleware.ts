bc69c0b04fef343840254e764180ef974c1298c1
Saad <saad_eew@hotmail.com>
2025-07-22 00:10:15 +0500

    create auth middleware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,53 @@
+import { Context, Next } from "hono";
+import { getCookie } from "hono/cookie";
+import { verify } from "hono/jwt";
+import { db } from "../../lib/db";
+import { DecodedToken } from "../types";
+
+export const authMiddleware = async (c: Context, next: Next) => {
+  try {
+    const token = getCookie(c, "jwt");
+    if (!token) {
+      return c.json({ message: "Unauthorized - No token provided" }, 401);
+    }
+    let decodedToken;
+    try {
+      decodedToken = (await verify(
+        token,
+        process.env.JWT_SECRET!,
+      )) as DecodedToken;
+    } catch (error) {
+      return c.json(
+        {
+          message: "Unauthorized / invalid token provided",
+        },
+        401,
+      );
+    }
+    if (!decodedToken?.id) {
+      return c.json({ error: "Invalid token payload" }, 401);
+    }
+    const user = await db.user.findUnique({
+      where: {
+        id: decodedToken.id,
+      },
+      select: {
+        email: true,
+        username: true,
+        first_name: true,
+        last_name: true,
+        role: true,
+        profile_picture_url: true,
+        bio: true,
+      },
+    });
+    if (!user) {
+      return c.json({ message: "User not found" }, 404);
+    }
+    c.set("user", user);
+    await next();
+  } catch (error) {
+    console.error("Auth Middleware Error:", error);
+    return c.json({ message: "Internal server error in auth middleware" }, 500);
+  }
+};
