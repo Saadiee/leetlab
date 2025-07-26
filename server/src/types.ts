4deeb773d722f6294211ee1167f22625d86fcbea
Saad <saad_eew@hotmail.com>
2025-07-26 22:36:41 +0500

    create user type

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,3 +1,15 @@
+import { $Enums } from "./generated/prisma";
+
 export type DecodedToken = {
   id: string;
 };
+
+export type User = {
+  username: string;
+  email: string;
+  first_name: string | null;
+  last_name: string | null;
+  profile_picture_url: string | null;
+  bio: string | null;
+  role: $Enums.UserRole;
+} | null;
