273674882ff503ff4b9fd6c4e2578e8894ccfbe3
Saad <saad_eew@hotmail.com>
2025-07-27 15:50:02 +0500

    add new types

--- server/src/types.ts
+++ server/src/types.ts
@@ -14,18 +14,26 @@ export type User = {
   role: $Enums.UserRole;
 } | null;
 
-type Json = string | number | boolean | null | Json[] | { [key: string]: Json };
+export type Submission = {
+  source_code: string;
+  language_id: number;
+  stdin: string;
+  expected_output: string;
+};
 
-export type Problem = {
-  title: string;
-  description: string;
-  difficulty: $Enums.Difficulty; // Or simply 'easy' | 'medium' | 'hard' if not using Prisma enum
-  tags: string[];
-  examples: Json;
-  constraints: string;
-  testcases: Json;
-  codeSnippets: Json;
-  referenceSolutions: string;
-  hints?: string;
-  editorial?: string;
+export type TestCase = {
+  input: string;
+  output: string;
+};
+
+export type TokenObject = {
+  token: string;
+};
+
+export type Judge0Result = {
+  token: string;
+  language_id: number;
+  stdout: string | null;
+  stderr: string | null;
+  status_id: number;
 };
