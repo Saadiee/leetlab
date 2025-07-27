9b9b48d0c689a1719abe0a295cfb6586bba48730
Saad <saad_eew@hotmail.com>
2025-07-27 16:01:09 +0500

    create "createProblem" controller

--- server/src/controllers/problems.controller.ts
+++ server/src/controllers/problems.controller.ts
@@ -1,10 +1,15 @@
 import { Context } from "hono";
 import { db } from "../../lib/db";
-import { Problem, User } from "../types";
-import { getJudge0LanguageId, SupportedLanguage } from "../../lib/judge0.lib";
+import { Submission, TestCase, User } from "../types";
+import {
+  getJudge0LanguageId,
+  poolBatchResults,
+  submitBatch,
+  SupportedLanguage,
+} from "../../lib/judge0.lib";
 
 export const createProblem = async (c: Context) => {
-  const body: Problem = await c.req.json();
+  const body = await c.req.json();
   const {
     title,
     description,
@@ -25,8 +30,54 @@ export const createProblem = async (c: Context) => {
   try {
     for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
       const languageId = getJudge0LanguageId(language as SupportedLanguage);
+      if (!languageId) {
+        return c.json(
+          { error: `Language --> ${language} is not supported` },
+          400,
+        );
+      }
+      const submissions: Submission[] = testcases.map(
+        ({ input, output }: TestCase) => ({
+          source_code: solutionCode,
+          language_id: languageId,
+          stdin: input,
+          expected_output: output,
+        }),
+      );
+      const submissionResults = await submitBatch(submissions);
+      const tokens = submissionResults.map((res) => res.token);
+
+      const results = await poolBatchResults(tokens);
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        if (result.status_id !== 3) {
+          return c.json(
+            { error: `Testcase ${i + 1} failed for language: ${language}` },
+            400,
+          );
+        }
+      }
+
+      const newProblem = await db.problem.create({
+        data: {
+          title,
+          description,
+          difficulty,
+          tags,
+          examples,
+          constraints,
+          testcases,
+          codeSnippets,
+          referenceSolutions,
+          userId: user.id,
+        },
+      });
+      return c.json({ newProblem }, 201);
     }
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error creating problem:", error);
+    return c.json({ message: "Internal Server Error" }, 500);
+  }
 };
 
 export const getAllProblems = async (c: Context) => {};
