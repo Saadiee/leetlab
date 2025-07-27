58239682d683bbb587a48907697a8b83e54066e3
Saad <saad_eew@hotmail.com>
2025-07-27 15:47:50 +0500

    create submitBatch function

--- server/lib/judge0.lib.ts
+++ server/lib/judge0.lib.ts
@@ -1,3 +1,6 @@
+import axios, { AxiosResponse } from "axios";
+import { Judge0Result, Submission, TokenObject } from "../src/types";
+
 export type SupportedLanguage = "PYTHON" | "JAVA" | "JAVASCRIPT";
 
 export const getJudge0LanguageId = (
@@ -10,4 +13,21 @@ export const getJudge0LanguageId = (
   };
 
   return languageMap[language] ?? null;
+};
+
+export const submitBatch = async (
+  submissions: Submission[],
+): Promise<TokenObject[]> => {
+  const { data }: AxiosResponse<TokenObject[]> = await axios.post(
+    `${process.env.JUDGE0_API_URL}/submissions/batch/?base64_encoded=false`,
+    { submissions },
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${process.env.SULU_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  return data;
 };
